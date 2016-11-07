3efa6ec45a9ae39359f87ac0ebec9337c05839a5
krissasantos <krissa.santos31@gmail.com>
2016-11-07 04:38:39 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,46 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
 
-// call functions here
+function frameIt() {
+  $('img').on('load', function(){
+      $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+
+  $('#typing').on('keydown', function(key){
+    if (key.which === 71){
+      alert('You pressed g!')
+    }
+    
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now." )
+  })
+}
+
+$(document).ready(function(){
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
+  // call functions here
+
+  //sample in reading:
+  // $(document).on('keypress', function(){
+  //   if (key.which == 82){
+  //     alert('s pressed')
+  //   }
+  // });
+
 
 });
